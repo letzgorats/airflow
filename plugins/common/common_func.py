3febc4cba10bc0d8bb14d8d9f0aed0002298133a
root <root@JYwindow>
2023-08-24 18:36:38 -0700

    op_kwargs example dag upload

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -12,7 +12,7 @@ def register2(name,gender,*args,**kwargs):
     print(f'성별:{gender}')
     print(f'기타옵션들:{args}')
     email = kwargs['email'] or None
-    phone = kwargs['email'] or None
+    phone = kwargs['phone'] or None
     if email:
         print(email)
     if phone:
