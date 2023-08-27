13252a9f192df59d8493ed602ffe0f0e565dfffc
root <root@JYwindow>
2023-08-27 12:30:15 -0700

    python operator template upload

---
+++ dags/dags_python_template.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.python import PythonOperator
+from airflow.decorators import task
+
+with DAG(
+    dag_id = "dags_python_template",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2023, 8, 10, tz="UTC"),
+    catchup=False
+) as dag:
+    
+    # 1번 방식
+    def python_function(start_date, end_date, **kwargs):
+        print(start_date)
+        print(end_date)
+
+    python_t1 = PythonOperator(
+        task_id = "python_t1",
+        python_callable=python_function,
+        op_kwargs={'start_date':'{{data_interval_start | ds}}','end_date':'{{data_interval_end | ds}}'}
+    )
+
+    # 2번 방식
+
+    @task(task_id = 'python_t2')
+    def python_function2(**kwargs):
+        print(kwargs)
+        print('ds:' + kwargs['ds'])
+        print('ts:' + kwargs['ts'])
+        print('data_interval_start:' + str(kwargs['data_interval_start']))
+        print('data_interval_end:' + str(kwargs['data_interval_end']))
+        print('task_instance:' + str(kwargs['ti']))
+
+    python_t1 >> python_function2()
