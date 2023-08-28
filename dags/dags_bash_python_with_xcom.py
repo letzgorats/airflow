8ca395a1e609ce349ff41a9482957279f687a595
root <root@JYwindow>
2023-08-28 14:35:51 -0700

    xcom pull&push with python&bash operators

--- dags/dags_bash_python_with_xcom.py
+++ dags/dags_bash_python_with_xcom.py
@@ -20,7 +20,7 @@ with DAG(
         env = {
             'STATUS':'{{ti.xcom_pull(task_ids="python_push")["status"]}}',
             'DATA':'{{ti.xcom_pull(task_ids="python_push")["data"]}}',
-            'OPTIONS_CNT':'{{ti.xcom_pull(task_ids="python_push")["oprionts_cnt"]}}'
+            'OPTIONS_CNT':'{{ti.xcom_pull(task_ids="python_push")["options_cnt"]}}'
         },
         bash_command = 'echo $STATUS && echo $DATA && echo $OPTIONS_CNT'
     )
